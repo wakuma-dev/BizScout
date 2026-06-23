203f9802cffe039ae4f815eb618e180998c45199
wakuma-dev <wakuma.hailu.dev@gmail.com>
2026-06-23 16:30:17 +0300

    fix event problem

--- src/pages/form/PartnerForm.tsx
+++ src/pages/form/PartnerForm.tsx
@@ -1,5 +1,5 @@
 import { Helmet } from "react-helmet-async";
-import { useReducer, FormEvent } from "react";
+import { useReducer, type FormEvent} from "react";
 
 type InputProps = {
   email: string;
