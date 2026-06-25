09d52c71ec258e96d4eb80afa700b6bb0334d66b
wakuma-dev <wakuma.hailu.dev@gmail.com>
2026-06-25 10:34:02 +0300

    feat(partner) working partenr section

--- src/pages/partners/Partners.tsx
+++ src/pages/partners/Partners.tsx
@@ -1,9 +1,19 @@
-
-
+import { Helmet } from 'react-helmet-async'
+import Hero from './components/Hero'
+import PartnersCta from './components/PartnersCta'
+import PartnersCategories from './components/PartnersCategories'
 const Partners = () => {
   return (
-    <div>Partners</div>
+    <main>
+    <Helmet>
+      <title>Partners | BizScout - Lenders, Advisors & Tools</title>
+    </Helmet>
+    <Hero />
+    <PartnersCategories />
+    <PartnersCta />
+    </main>
+
   )
 }
 
-export default Partners
+export default Partners 
